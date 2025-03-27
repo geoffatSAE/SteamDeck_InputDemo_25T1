d63c46c1f1fecbaf72e9bbc31b284b27dd64d495
Geoff Hill @ SAE Institute <g.hill@sae.edu>
2025-03-27 14:38:01 +0000

    Initial Project Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    PlayerInput playerInput; //the player input system we're reading from
+    InputAction moveInput, fireInput, exitInput;
+    public float moveSpeed;
+
+    void Awake(){
+
+        playerInput = new PlayerInput();
+
+    }
+
+    void OnEnable(){
+
+        moveInput = playerInput.Player.Move;
+        moveInput.Enable();
+
+        fireInput = playerInput.Player.Fire;
+        fireInput.Enable();
+        fireInput.performed += Fire;
+
+        exitInput = playerInput.Player.Exit;
+        exitInput.Enable();
+        exitInput.performed += Quit;
+    }
+
+    void OnDisable(){
+
+        moveInput.Disable();
+        fireInput.Disable();
+        exitInput.Disable();
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 vectorInput = moveInput.ReadValue<Vector2>();
+
+        //Debug.Log("input is " + vectorInput.x + " and " + vectorInput.y);
+        transform.Translate(new Vector3 (vectorInput.x, 0, vectorInput.y) * Time.deltaTime * moveSpeed);
+
+
+    }
+
+
+    void Fire(InputAction.CallbackContext context){
+        
+        Debug.Log("pew pew pew");
+    }
+
+    void Quit(InputAction.CallbackContext context){
+
+
+                Debug.Log("Exiting the app ... alt f4");
+                Application.Quit();
+    }
+}
